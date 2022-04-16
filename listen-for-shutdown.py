66a7f73a8b92bafafd84806d6b872d3c6d27a476
Hunter Garza <Hg155801@tamu.edu>
2022-04-15 22:49:43 -0500

    Added comments and GPIO values

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,9 +4,18 @@
 import RPi.GPIO as GPIO
 import subprocess
 
-
+#Use BCM numbers (GPIO #'s)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
 
+#Initial GPIO values on boot
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.OUT, intitial=GPIO.HIGH)
+GPIO.setup(17, GPIO.OUT, intitial=GPIO.HIGH)
+
+#New GPIO values after shutdown
+GPIO.wait_for_edge(3, GPIO.FALLING)
+GPIO.output(4, GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
+
+#Pi Shutdown
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
