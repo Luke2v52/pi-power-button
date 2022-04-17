96c0537bb9f6eb57db0210805eca7dcfcb7fc938
Hunter Garza <Hg155801@tamu.edu>
2022-04-16 20:35:44 -0500

    fixed typo

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -9,8 +9,8 @@ GPIO.setmode(GPIO.BCM)
 
 #Initial GPIO values on boot
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(4, GPIO.OUT, intitial=GPIO.HIGH)
-GPIO.setup(17, GPIO.OUT, intitial=GPIO.HIGH)
+GPIO.setup(4, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(17, GPIO.OUT, initial=GPIO.HIGH)
 
 #New GPIO values after shutdown
 GPIO.wait_for_edge(3, GPIO.FALLING)
